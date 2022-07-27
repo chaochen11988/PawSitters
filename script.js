20a17e78f4ab314ae28524fe2d8a2da74a666456
Chao Chen <chaochen11988@gmail.com>
2022-07-27 12:16:01 -0400

    addition of services images and feedback form

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 hamburgerButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active")
 })
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "chaochen11988testing@gmail.com",
+        Password: "ruL7iU8aZSFd4F5",
+        To: 'chaochen11988testing@gmail.com',
+        From: document.getElementById("email").value,
+        Subject: "Feedback and suggestions",
+        Body: "First name: " + document.getElementById("fName").value +
+            "<br> Last name: " + document.getElementById("lName").value +
+            "<br> Email address: " + document.getElementById("email").value +
+            "<br> Phone number: " + document.getElementById("phone").value +
+            "<br> Message: " + document.getElementById("message").value +
+    }).then(
+        message => alert("Email Sent Successfully")
+    );
+}
