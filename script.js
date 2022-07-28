a6ddbab9fcdff682888527537ae2ed2c7bbda189
Chao Chen <chaochen11988@gmail.com>
2022-07-27 23:43:26 -0400

    Updated contact form to include email validation and console out a message for a successful submission

--- script.js
+++ script.js
@@ -6,23 +6,27 @@ hamburgerButton.addEventListener("click", () => {
 })
 
 function sendEmail() {
-    Email.send({
-        //working progress//
-        Host: "smtp.gmail.com",
-        Username: "chaochen11988testing@gmail.com",
-        Password: "ruL7iU8aZSFd4F5",
-        To: 'chaochen11988testing@gmail.com',
-        From: document.getElementById("email").value,
-        Subject: "Feedback and suggestions",
-        Body: "First name: " + document.getElementById("fName").value +
-            "<br> Last name: " + document.getElementById("lName").value +
-            "<br> Email address: " + document.getElementById("email").value +
-            "<br> Phone number: " + document.getElementById("phone").value +
-            "<br> Service: " + document.getElementById("contact-us-services").value +
-            "<br> Start date: " + document.getElementById("startDate").value +
-            "<br> End date: " + document.getElementById("endDate").value +
-            "<br> Message: " + document.getElementById("message").value
-    }).then(
-        message => alert("Email Sent Successfully")
-    );
+    let inputFName = document.getElementById("fName").value;
+    if (!document.getElementById("email").value.includes("@")) {
+        alert("Email address field must contain @ sign, please input a valid email.");
+    } else {
+        Email.send({
+            //working progress//
+            Host: "smtp.gmail.com",
+            Username: "chaochen11988testing@gmail.com",
+            Password: "ruL7iU8aZSFd4F5",
+            To: 'chaochen11988testing@gmail.com',
+            From: document.getElementById("email").value,
+            Subject: "Feedback and suggestions",
+            Body: "First name: " + document.getElementById("fName").value +
+                "<br> Last name: " + document.getElementById("lName").value +
+                "<br> Email address: " + document.getElementById("email").value +
+                "<br> Phone number: " + document.getElementById("phone").value +
+                "<br> Service: " + document.getElementById("contact-us-services").value +
+                "<br> Start date: " + document.getElementById("startDate").value +
+                "<br> End date: " + document.getElementById("endDate").value +
+                "<br> Message: " + document.getElementById("message").value
+        }).then(
+            message => alert("Thank you for your inquiry," + " " + inputFName + "." + "\nI'll get back to you as soon as I can!"));
+    }
 }
